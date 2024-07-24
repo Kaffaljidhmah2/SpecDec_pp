844fae54fa0a7c060e26dbf2f357e365c1e30876
Hacky Huang <35477353+Kaffaljidhmah2@users.noreply.github.com>
2024-07-24 12:37:17 -0700

    Update gen_data.sh

--- data/gen_data.sh
+++ data/gen_data.sh
@@ -13,7 +13,7 @@ TMP2=${OUT}/tmp/tmp2.json
 TMP3=${OUT}/tmp/tmp3.json
 TMP4=${OUT}/tmp/tmp4.json
 
-python3 gen_dataset.py --dataset_name ${DATASET} --model_name ${TARGET} --mode hf --do_sample  --n_begin ${NBEGIN} --n_end ${NEND} --output_file $TMP1 
+python3 gen_dataset.py --dataset_name ${DATASET} --model_name ${TARGET} --mode hf --do_sample --output_file $TMP1 
 python3 gen_assistant.py --model_name ${DRAFT} --do_sample --input_file $TMP1 --output_file $TMP2 
 python3 gen_log_p.py --model_name ${DRAFT} --input_file $TMP2 --output_file $TMP3 
 python3 gen_log_p.py --model_name ${TARGET} --input_file $TMP3 --output_file $TMP4 
